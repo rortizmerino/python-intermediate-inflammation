34c8e94edfd2a0e7a2590bb96face077c09482ae
Raul Ortiz <raul.ortiz@tudelft.nl>
2024-04-15 16:04:24 +0200

    Added parametrised test

--- tests/test_models.py
+++ tests/test_models.py
@@ -29,6 +29,16 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    from inflammation.models import daily_mean
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
 def test_daily_max():
     """Test that max function works for an array of positive integers."""
