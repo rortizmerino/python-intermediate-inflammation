b56525a44bffde81953c628f9b0d5cdeab504d97
Raul Ortiz <raul.ortiz@tudelft.nl>
2024-04-15 15:58:51 +0200

    Added type test

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import numpy.testing as npt
-
+import pytest
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -53,3 +53,12 @@ def test_daily_min():
     test_result = np.array([-4, -6, 2])
 
     npt.assert_array_equal(daily_min(test_input), test_result)
+
+
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
