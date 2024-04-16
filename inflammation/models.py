5b0403b35efc902f0d0b01dfaffc234a92feef56
Raul Ortiz <raul.ortiz@tudelft.nl>
2024-04-16 11:52:42 +0200

    Added std as new functional requirement in new feature branch

--- inflammation/models.py
+++ inflammation/models.py
@@ -32,3 +32,7 @@ def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array."""
     return np.min(data, axis=0)
 
+
+def daily_std(data):
+    """Calculate the daily std of a 2D inflammation data array."""
+    return np.std(data, axis=0)
