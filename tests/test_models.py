6c5189856eaacd7c6be7a56d9ef7ba957dab3f50
Raúl Ortiz <41450568+rortizmerino@users.noreply.github.com>
2026-05-26 13:52:16 +0200

    Parametrized daily_max

--- tests/test_models.py
+++ tests/test_models.py
@@ -19,21 +19,14 @@ def test_daily_mean(test_input, test_result):
     """Test than mean function works for both zeroes and integers"""
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+@pytest.mark.parametrize(
+        "test_input, test_result",
+        [
+            ([[1, 2], [3, 4], [5, 6]], [5, 6]),
+            ([[1, 2, -9], [-3, 4, -2], [-1, 5, -6]], [1, 5, -2]),
+        ])
 
-
-def test_daily_max_string():
-    """ Test for TypeError when parsing strings """
-
-    with pytest.raises(TypeError):
-        error_expected = daily_max(['Hello', 'there'])
-
-def test_daily_max_integers():
-    """Test that max function works for an array of positive integers."""
-
-    test_input = np.array([[1, -2],
-                           [3, -4],
-                           [-5, -6]])
-    test_result = np.array([3, -2])
-
-    # Need to use Numpy testing functions to compare arrays
+def test_daily_max(test_input, test_result):
+    """Test that max function works for an array of positive and negative integers."""
     npt.assert_array_equal(daily_max(test_input), test_result)
+
