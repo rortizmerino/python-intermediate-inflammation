a2c7824b3901f4955fd7c6d9f88ee0b72687865d
Raúl Ortiz <41450568+rortizmerino@users.noreply.github.com>
2026-05-26 13:32:47 +0200

    Add parametrized test for daily mean

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,35 +1,30 @@
 """Tests for statistics functions within the Model layer."""
 
+import pytest
 import numpy as np
 import numpy.testing as npt
 
 from inflammation.models import daily_mean, daily_max
 
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
-    
+@pytest.mark.parametrize(
+        "test_input, test_result",
+        [
+            ([[0,0], [0,0], [0,0]], [0,0]),
+            ([[1,2], [3,4], [5,6]], [3,4]),
+        ]
+)
 
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
-
-    # Need to use Numpy testing functions to compare arrays
+def test_daily_mean(test_input, test_result):
+    """Test than mean function works for both zeroes and integers"""
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
 
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
 
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([3, 4])
+def test_daily_max_string():
+    """ Test for TypeError when parsing strings """
 
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
-
-
+    with pytest.raises(TypeError):
+        error_expected = daily_max(['Hello', 'there'])
 
 def test_daily_max_integers():
     """Test that max function works for an array of positive integers."""
