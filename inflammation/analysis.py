1cbbfa5cb2e0cf7ec70748ba06de671b120cbd08
Raúl Ortiz <41450568+rortizmerino@users.noreply.github.com>
2026-05-27 13:44:53 +0200

    Refactored analysis.py to have pure function

--- inflammation/analysis.py
+++ inflammation/analysis.py
@@ -40,8 +40,10 @@ def analyse_data(data_source):
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
     data = data_source.load_inflammation_data()
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
+    return daily_standard_deviation
 
-
+def compute_standard_deviation_by_day(data):
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
 
