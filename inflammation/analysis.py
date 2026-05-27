c3108fe77cdf79b897fb452520af01f051503f83
catactg <catactg@gmail.com>
2026-05-27 11:53:38 +0200

    added CSVDataSource in analyse_data and test

--- inflammation/analysis.py
+++ inflammation/analysis.py
@@ -20,13 +20,12 @@ class CSVDataSource:
         return data
 
 
-def analyse_data(data_dir):
+def analyse_data(data_source):
     """Calculates the standard deviation by day between datasets.
 
     Gets all the inflammation data from CSV files within a directory,
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
-    data_source = CSVDataSource(data_dir=data_dir)
     data = data_source.load_inflammation_data()
 
 
